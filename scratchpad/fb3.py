14e0e853df212ba79950fac9f7b0f97a255b0cc3
Mike Tigas <mike@tig.as>
2010-07-28 16:57:46 -0700

    Remove previous sample project.

--- scratchpad/fb3.py
+++ scratchpad/fb3.py
@@ -1,8 +1,8 @@
 import json
 from geopy import geocoders
 from os.path import exists
-from home.models import Point
 from django.contrib.gis.geos import fromstr as geo_from_str
+from gheat_demo.friends.models import FriendPoint
 
 if __name__ == "__main__":
     if exists('/Users/mtigas/Code/heatmap-mt/fb.cache.json'):
@@ -31,20 +31,8 @@ if __name__ == "__main__":
                         place, (lat,lon) = geocoder.geocode(friend['location']['name'])
                         coord = (lon,lat)
                         cache[friend['location']['name']] = coord
-                    p = Point(
-                        uid=f_id,
+                    p = FriendPoint(
+                        name=u"%s" % f_id,
                         geometry=geo_from_str('POINT(%s %s)' % (coord[0],coord[1]))
                     )
                     p.save()
-
-    f = open('friends2.json')
-    j = json.loads(f.read())
-    f.close()
-    
-    f = open('fb.cache.json','w')
-    f.write(json.dumps(cache))
-    f.close()
-
-    hm = heatmap.Heatmap()
-    hm.heatmap(points, "fb.png",dotsize=65,size=(6000,4000), scheme='classic')
-    hm.saveGMAP3JS()
