0d3d87ab5a6df17c74e0d85a2f6eb8c412fbc291
Mike Tigas <mike@tig.as>
2010-07-29 02:32:50 -0500

    Tweaks to FileSystemBackend

--- src/gheat/storage_backend/base.py
+++ src/gheat/storage_backend/base.py
@@ -50,10 +50,17 @@ class FileSystemStorage(BaseStorage):
                 "GHEAT_FILESYSTEM_STORAGE_DIR to be set.")
     
     def dir_for_tile(self, tile, mapname=""):
-        return os.path.join(self.storage_dir, mapname, tile.color_scheme)
+        """
+        Returns the directory path that the given tile should be stored in.
         
+        i.e. /tmp/gheat/defaultmap/firetrans/
+        """
+        return os.path.join(self.storage_dir, mapname, tile.color_scheme)
     
     def path_for_tile(self, tile, mapname=""):
+        """
+        Returns the path that should correspond to this tile file on the filesystem.
+        """
         tiledir = self.dir_for_tile(tile, mapname)
         return os.path.join(tiledir, "%s-%s,%s.png" % (tile.zoom, tile.x, tile.y))
     
@@ -62,26 +69,29 @@ class FileSystemStorage(BaseStorage):
         return os.path.exists(filepath)
         
     def get_tile(self, tile, mapname=""):
+        # If the cache file for this tile does not exist
         if not self.has_tile(tile, mapname):
-            fileobj = tile.generate()
-            
             # Generate the storage directory for this tile, if it doesn't exist
             tiledir = self.dir_for_tile(tile, mapname)
             if not os.path.exists(tiledir):
                 os.makedirs(tiledir)
             
-            # Open the output file and write to it.
+            # Generate the image
+            fileobj = tile.generate()
+            
+            # Open the output file and write our data to it.
             tilepath = self.path_for_tile(tile, mapname)
             outfile = open(tilepath, 'wb')
             for line in fileobj:
                 outfile.write(line)
             outfile.close()
-            fileobj.seek(0)
             
+            # Rewind our in-memory image pseudofile and return it
+            fileobj.seek(0)
             return fileobj
         else:
+            # Cache file exists, so return a file pointer to that
             tilepath = self.path_for_tile(tile, mapname)
-            print tilepath
             tilefile = open(tilepath, 'rb')
             return tilefile
     
@@ -89,10 +99,13 @@ class FileSystemStorage(BaseStorage):
         storage_dir = os.path.join(self.storage_dir, "empty_tiles")
         empty_tile_path = os.path.join(storage_dir, "%s-empty.png" % tile.color_scheme)
         
+        # If the empty tile for this scheme is not cached
         if not os.path.exists(empty_tile_path):
+            # Generate the empty_tiles storage directory
             if not os.path.exists(storage_dir):
-                os.makedirs(tiledir)
-                
+                os.makedirs(storage_dir)
+            
+            # Generate our empty tile
             fileobj = tile.get_empty()
             
             # Open the output file and write to it.
@@ -100,10 +113,12 @@ class FileSystemStorage(BaseStorage):
             for line in fileobj:
                 outfile.write(line)
             outfile.close()
-            fileobj.seek(0)
             
+            # Rewind our in-memory image pseudofile and return it
+            fileobj.seek(0)
             return fileobj
         else:
+            # Cache file exists, so return a file pointer to that
             tilefile = open(empty_tile_path, 'rb')
             return tilefile
 
