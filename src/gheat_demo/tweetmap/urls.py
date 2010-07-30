40ab38a39c064f15e1db40a0fffd16f8b2b6d526
Mike Tigas <mike@tig.as>
2010-07-30 00:59:42 -0500

    Show a tweet counter (just to give an idea of the data density).

--- src/gheat_demo/tweetmap/urls.py
+++ src/gheat_demo/tweetmap/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from gheat_demo.tweetmap.models import TweetPoint
 
 urlpatterns = patterns('gheat_demo.tweetmap.views',
     # The basic HTML page that serves as our map view.
@@ -10,7 +11,10 @@ urlpatterns = patterns('gheat_demo.tweetmap.views',
         name    = 'home',
         kwargs  = {
             'template': 'home.html',
+            'extra_context': {
+                'tweet_count':TweetPoint.objects.count()
             }
+        }
     ),
     # URL pattern that serves tiles at the path that Google Maps API expects
     # ex.: /tweetmap/tiles/fire/12/3,2.png
