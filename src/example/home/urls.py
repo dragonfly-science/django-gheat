7aa6dd1fdb4a69c219cc9503139ddb40952a9681
Edward Abraham <edward@dragonfly.co.nz>
2012-04-20 05:43:36 +1200

    change example home to gmap

--- src/example/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.base import TemplateView
-from django.conf import settings
-
-
-urlpatterns = patterns('home.views',
-    (r'^$', TemplateView.as_view(template_name= 'home.html')),
-)
