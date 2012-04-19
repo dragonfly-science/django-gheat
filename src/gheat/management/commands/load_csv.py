1bc3d48bbdabf435de53f14ab6b0d8aa92616c75
Edward Abraham <edward@dragonfly.co.nz>
2012-04-20 11:56:03 +1200

    Got load_csv working in the management command

--- src/gheat/management/commands/load_csv.py
+++ src/gheat/management/commands/load_csv.py
@@ -2,6 +2,8 @@ import csv
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.gis.geos import GEOSGeometry
 from django.db import connection, transaction
+from tempfile import TemporaryFile
+from optparse import make_option
 
 
 from gheat.default_settings import GHEAT_POINT_MODEL
@@ -11,50 +13,33 @@ module = '.'.join(GHEAT_POINT_MODEL.split('.')[:-1])
 pointclass = GHEAT_POINT_MODEL.split('.')[-1]
 Point = getattr(__import__(module, fromlist=[pointclass]), pointclass)
 
-class PointReader(object):
-    def __init__(self, filename):
-        self.fid = open(filename)
-        self.reader = csv.DictReader(self.fid)
-        self.count = 0
-
-    def readline(self):
-        try:
-            row = self.reader.next()
-            lat = float(row['lat'])
-            lng = float(row['long'])
-            text = "%0.5f\t%0.5f\n" % (lng, lat)
-            self.count += 1
-            print self.count, text[:-1]
-            return text
-        except StopIteration:
-            return ''
-
-    def read(self, n=-1):
-        text = ''
-        new = True
-        while new and n > 0 and len(text) < n:
-            new = self.readline()
-            text += new
-        if n >= 0 and len(text) > n:
-            text = text[:n]
-        return text
-
-    def next():
-        while True:
-            new = self.readline()
-            if new:
-                return new
-            else:
-                raise StopIteration
-
 class Command(BaseCommand):
     args = '<filename>'
     help = 'Import data from the csv file "filename", which has columns lat and long.'
-
+    option_list = BaseCommand.option_list + (
+        make_option('--lat',
+            action = 'store',
+            dest = 'lat',
+            default = 'lat',
+            help = 'Name of the latitude column'),
+        make_option('--long',
+            action = 'store',
+            dest = 'long',
+            default = 'long',
+            help = 'Name of the longitude column'),
+        )
+    
     def handle(self, *args, **options):
+        tempfile = TemporaryFile('w+r')
+        reader = csv.DictReader(open(args[0]))
+        for row in reader:
+            lat = float(row[options['lat']])
+            lng = float(row[options['long']])
+            tempfile.write("%0.5f\t%0.5f\n" % (lng, lat))
+        tempfile.seek(0)
         cursor = connection.cursor()
         cursor.execute('CREATE TEMPORARY TABLE _point_import (lat FLOAT, long FLOAT);')
-        cursor.copy_from(PointReader(args[0]), '_point_import')
+        cursor.copy_from(tempfile, '_point_import')
         cursor.execute("INSERT INTO %s (geometry) SELECT ST_SetSRID(ST_MakePoint(long, lat), 4326) AS geometry FROM _point_import;" % Point._meta.db_table)
         transaction.commit_unless_managed()
 
